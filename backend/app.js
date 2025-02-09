be55797a24c01e7d009bd6a994b052e4ec644eac
John Germing <johngerming8@gmail.com>
2025-02-09 15:35:58 -0800

    Completed backend, initializing frontend

--- backend/app.js
+++ backend/app.js
@@ -8,13 +8,15 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to accept JSON in the req.body
 
+// Prefixing all the routes with /api/products
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log(`Server started at http://localhost:${PORT}`);
 });
 
