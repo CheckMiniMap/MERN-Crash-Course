2a9ec72d284da34130d7408052a28122ce56ccad
John Germing <johngerming8@gmail.com>
2025-02-12 04:45:43 -0800

    Created base pages for routes with chakra ui setup

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+// rafce will auto-generate the file's basic code.
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default HomePage
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: '127.0.0.1', // force IPv4
+    port: 3000,        // try a different port
+    strictPort: false, // allow Vite to choose another free port if 3000 is taken
+  },
+  plugins: [react(), tsconfigPaths()],
 })
