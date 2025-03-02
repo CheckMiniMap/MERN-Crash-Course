c5cde855080c95216414aa9e100bd8b708f2faf2
John Germing <johngerming8@gmail.com>
2025-03-02 14:17:27 -0800

    Starting deployment

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
-// Tutorial: https://www.youtube.com/watch?v=O3BUHwfHf84 1:57:54
+// Tutorial: https://www.youtube.com/watch?v=O3BUHwfHf84 2:02:21
 // const express = require('express'); <-- common way of importing express
 import express from "express"; // Modern way of importing express with package.json having "type": "module"
 import dotenv from "dotenv";
+import path from "path";
+
 import { connectDB } from './config/db.js';
 import productRoutes from "./routes/product.route.js";
 
@@ -10,10 +12,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows us to accept JSON in the req.body
 
 // Prefixing all the routes with /api/products
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {}
 
 app.listen(PORT, () => {
   connectDB();
